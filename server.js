f800c7836f8f92ffb979f002820a8b8fe7aa9af8
sulakshana borsune <sulakshana.borsune@gmail.com>
2020-03-15 16:13:02 -0700

    one more change in routes

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 app.get('./routes')
 
-app.use(require('./routes'))
+//app.get(require('./routes'))
 
 app.get('/', (request, response) => {
   response.sendFile(join(__dirname, 'public', 'index.html'))
