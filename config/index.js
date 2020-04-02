06743a125f2648868f3d0c424439650698c5d801
sulakshana borsune <sulakshana.borsune@gmail.com>
2020-04-02 12:34:31 -0700

    added dotenv in server.js

--- config/index.js
+++ config/index.js
@@ -3,6 +3,6 @@ const {connect} = require('mongoose')
 module.exports = connect( process.env.MONGODB_URI || process.env.LOCAL_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false,
+  useFindAndModify: true,
   useCreateIndex: true
 })
